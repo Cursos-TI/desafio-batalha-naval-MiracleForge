dc2c76133513167bca67f9f532f68a4fe5878361
paulodev <paulomoreirarosado@hotmail.com>
2025-05-06 14:15:53 -0300

    Feat(Nivel Mestre): Concuido nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,72 +4,162 @@
 #define num_of_collums 10
 #define ship_size 3
 #define pos_vertical 1
-// desafio batalha naval - matecheck
-// este código inicial serve como base para o desenvolvimento do sistema de
-// batalha naval. siga os comentários para implementar cada parte do desafio.
+//_desafio batalha naval - matecheck
+// este código inicial serve como base para o_desenvolvimento do sistema de
+// batalha naval. siga os comentários para implementar cada parte_do desafio.
 //
 void add_ship_vertical(int tabuleiro[num_of_rows][num_of_collums], int coord_x,
                        int coord_y) {
-  for (int v = 0; v < ship_size; v++) {
+  for (int _v = 0; _v < ship_size; _v++) {
     if (coord_x + ship_size > num_of_rows ||
-        tabuleiro[coord_x + v][coord_y] == 3) {
+        tabuleiro[coord_x + _v][coord_y] == 3) {
       printf("coordenada inválida");
       exit(1);
     }
-    tabuleiro[coord_x + v][coord_y] = 3;
+    tabuleiro[coord_x + _v][coord_y] = 3;
   }
 }
 void add_ship_horizontal(int tabuleiro[num_of_rows][num_of_collums],
                          int coord_x, int coord_y) {
-  for (int h = 0; h < ship_size; h++) {
+  for (int _h = 0; _h < ship_size; _h++) {
     if (coord_y + ship_size > num_of_collums ||
-        tabuleiro[coord_x][coord_y + h] == 3) {
+        tabuleiro[coord_x][coord_y + _h] == 3) {
       printf("coordenada inválida");
       exit(1);
     }
-    tabuleiro[coord_x][coord_y + h] = 3;
+    tabuleiro[coord_x][coord_y + _h] = 3;
   }
 }
 
 void add_ship_diag(int tabuleiro[num_of_rows][num_of_collums], int coord_x,
                    int coord_y) {
-  for (int d = 0; d < ship_size; d++) {
-    if (coord_x - d < 0 || coord_x - d >= num_of_rows || coord_y + d < 0 ||
-        coord_y + d >= num_of_collums) {
+  for (int _d = 0; _d < ship_size; _d++) {
+    if (coord_x - _d < 0 || coord_x - _d >= num_of_rows || coord_y + _d < 0 ||
+        coord_y + _d >= num_of_collums) {
       printf(
-          "Coordenada inválida: o navio ultrapassa os limites do tabuleiro.\n");
+          "coordenada inválida: o navio ultrapassa os limites_do tabuleiro.\n");
       exit(1);
     }
 
-    if (tabuleiro[coord_x - d][coord_y + d] == 3) {
-      printf("Coordenada inválida: há um navio nesta posição.\n");
+    if (tabuleiro[coord_x - _d][coord_y + _d] == 3) {
+      printf("coordenada inválida: há um navio nesta posição.\n");
       exit(1);
     }
   }
 
-  for (int d = 0; d < ship_size; d++) {
-    tabuleiro[coord_x - d][coord_y + d] = 3;
+  for (int _d = 0; _d < ship_size; _d++) {
+    tabuleiro[coord_x - _d][coord_y + _d] = 3;
   }
 }
 
 void draw_table(int tabuleiro[num_of_rows][num_of_collums]) {
   printf("  / | ");
-  for (int c = 0; c < num_of_collums; c++) {
-    printf("%c | ", 'a' + c);
+  for (int _c = 0; _c < num_of_collums; _c++) {
+    printf("%c | ", 'a' + _c);
   }
   printf("\n");
 
-  for (int r = 0; r < num_of_rows; r++) {
-    printf("  %i | ", r);
-    for (int c = 0; c < num_of_collums; c++) {
-      printf("%i | ", tabuleiro[r][c]);
+  for (int _r = 0; _r < num_of_rows; _r++) {
+    printf("  %i | ", _r);
+    for (int _c = 0; _c < num_of_collums; _c++) {
+      printf("%i | ", tabuleiro[_r][_c]);
     }
     printf("\n");
   }
 }
 
+void add_power_cone(int tabuleiro[num_of_rows][num_of_collums], int coord_x,
+                    int coord_y) {
+  // Matriz em formato de cone: 3 linhas por 5 colunas, simulando expansão para
+  // baixo
+
+  int _cone[3][5] = {
+      {0, 0, 1, 0, 0},
+      {0, 1, 1, 1, 0},
+      {1, 1, 1, 1, 1},
+  };
+
+  for (int _tx = 0; _tx < 3; _tx++) {
+    int x = coord_x + _tx;
+    for (int _ty = 0; _ty < 5; _ty++) {
+      int y = coord_y + _ty;
+      // Detecta se as coordenadas estão fora do limite da tabela
+      if (x >= 0 && x < num_of_rows && y >= 0 && y < num_of_collums) {
+        // caso seja detectada uma celula com navio e o valor na matrix do poder
+        // seja um 1, aplica-se o valor 5 a tabela ao inves do valor da matriz
+        if (tabuleiro[coord_x + _tx][coord_y + _ty] == 3 &&
+            _cone[_tx][_ty] == 1) {
+          tabuleiro[coord_x + _tx][coord_y + _ty] = 5;
+          // caso não seja um navio aplicar o valor da matri do poder
+        } else if (tabuleiro[coord_x + _tx][coord_y + _ty] != 3) {
+          tabuleiro[coord_x + _tx][coord_y + _ty] = _cone[_tx][_ty];
+        }
+      }
+    }
+  }
+}
+
+void add_power_crux(int tabuleiro[num_of_rows][num_of_collums], int coord_x,
+                    int coord_y) {
+  int _cruz[3][5] = {
+      {0, 0, 1, 0, 0},
+      {1, 1, 1, 1, 1},
+      {0, 0, 1, 0, 0},
+  };
+
+  for (int _tx = 0; _tx < 3; _tx++) {
+    int x = coord_x + _tx;
+    for (int _ty = 0; _ty < 5; _ty++) {
+      int y = coord_y + _ty;
+      // Detecta se as coordenadas estão fora do limite da tabela
+      if (x >= 0 && x < num_of_rows && y >= 0 && y < num_of_collums) {
+        // caso seja detectada uma celula com navio e o valor na matrix do poder
+        // seja um 1, aplica-se o valor 5 a tabela ao inves do valor da matriz
+        if (tabuleiro[coord_x + _tx][coord_y + _ty] == 3 &&
+            _cruz[_tx][_ty] == 1) {
+          tabuleiro[coord_x + _tx][coord_y + _ty] = 5;
+        } else if (tabuleiro[coord_x + _tx][coord_y + _ty] != 3) {
+          tabuleiro[coord_x + _tx][coord_y + _ty] = _cruz[_tx][_ty];
+        }
+      }
+    }
+  }
+}
+
+void add_power_octraetro(int tabuleiro[num_of_rows][num_of_collums],
+                         int coord_x, int coord_y) {
+  int _octraedro[3][5] = {
+      {0, 0, 1, 0, 0},
+      {0, 1, 1, 1, 0},
+      {0, 0, 1, 0, 0},
+  };
+
+  for (int _tx = 0; _tx < 3; _tx++) {
+    int x = coord_x + _tx;
+    for (int _ty = 0; _ty < 5; _ty++) {
+      int y = coord_y + _ty;
+      // Detecta se as coordenadas estão fora do limite da tabela
+      if (x >= 0 && x < num_of_rows && y >= 0 && y < num_of_collums) {
+        // caso seja detectada uma celula com navio e o valor na matrix do poder
+        // seja um 1, aplica-se o valor 5 a tabela ao inves do valor da matriz
+        if (tabuleiro[coord_x + _tx][coord_y + _ty] == 3 &&
+            _octraedro[_tx][_ty] == 1) {
+          tabuleiro[coord_x + _tx][coord_y + _ty] = 5;
+        } else if (tabuleiro[coord_x + _tx][coord_y + _ty] != 3) {
+
+          if (tabuleiro[coord_x + _tx][coord_y + _ty] == 1) {
+            tabuleiro[coord_x + _tx][coord_y + _ty] = 1;
+          } else {
+            tabuleiro[coord_x + _tx][coord_y + _ty] = _octraedro[_tx][_ty];
+          }
+        }
+      }
+    }
+  }
+}
+
 int main() {
-  // nível novato - posicionamento dos navios
+  // nível novato - posicionamento_dos navios
   int tabuleiro[num_of_rows][num_of_collums] = {0};
   // navios
   add_ship_vertical(tabuleiro, 7, 6);
@@ -81,41 +171,13 @@ int main() {
   add_ship_diag(tabuleiro, 2, 4);
   add_ship_diag(tabuleiro, 7, 3);
 
+  add_power_cone(tabuleiro, 5, 5);
+
+  add_power_crux(tabuleiro, 1, 3);
+
+  add_power_octraetro(tabuleiro, 6, 1);
+
   draw_table(tabuleiro);
-  // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro
-  //
-  // (Ex: int tabuleiro[5][5];). Sugestão: Posicione dois navios no tabuleiro,
-  // um verticalmente e outro horizontalmente. Sugestão: Utilize `printf` para
-  // exibir as coordenadas de cada parte dos navios.
-
-  // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-  // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-  // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-  // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições
-  // vazias e 3 para posições ocupadas.
-
-  // Nível Mestre - Habilidades Especiais com Matrizes
-  // Sugestão: Crie matrizes para representar habilidades especiais como cone,
-  // cruz, e octaedro. Sugestão: Utilize estruturas de repetição aninhadas para
-  // preencher as áreas afetadas por essas habilidades no tabuleiro. Sugestão:
-  // Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não
-  // afetadas e 1 para áreas atingidas.
-
-  // Exemplos de exibição das habilidades:
-  // Exemplo para habilidade em cone:
-  // 0 0 1 0 0
-  // 0 1 1 1 0
-  // 1 1 1 1 1
-
-  // Exemplo para habilidade em octaedro:
-  // 0 0 1 0 0
-  // 0 1 1 1 0
-  // 0 0 1 0 0
-
-  // Exemplo para habilidade em cruz:
-  // 0 0 1 0 0
-  // 1 1 1 1 1
-  // 0 0 1 0 0
 
   return 0;
 }
